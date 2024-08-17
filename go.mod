e869106872d127072afdf4e2b741bcf8f91c8c65
sweeneyb <tab126@gmail.com>
2024-08-17 20:56:23 +0000

    working CLI and webapp

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/sweeneyb/implBasicGin
 
 go 1.13
+
+require github.com/gin-gonic/gin v1.10.0
--- main.go
+++ main.go
@@ -5,94 +5,100 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"net/http"
+    "github.com/gin-gonic/gin"
+	"bytes"
 )
 
 // These are our handler functions.
-func doGet(what Context) {
-	what.String(200, "GET: %v\n", what.FullPath())
+func doGet(c *gin.Context) {
+	c.String(200, "GET: %v\n", c.Request.URL.Path)
 }
 
-func doPost(what Context) {
-	what.String(200 , "POST: %v\n", what.FullPath())
+func doPost(c *gin.Context) {
+	c.String(200 , "POST: %v\n", c.Request.URL.Path)
 }
 
 // This is the "main" where we configure our framework to handle the CLI commands
 func main() {
 	reader := bufio.NewReader(os.Stdin)
+	
+	// the router is what directs the get/post to the handlers
+	router := router{handlers: map[string] map[string] func(*gin.Context){}}
+	router.configureGET( "/", doGet)
+	router.configurePOST("/", doPost)
 
+	gin.SetMode(gin.ReleaseMode)
+	ginRouter := gin.Default()
+	ginRouter.GET("/", doGet)
+	
+	go func() { ginRouter.Run("localhost:8080") }()
 	for {
 		fmt.Print("> ")
 		input, _ := reader.ReadString('\n')
 		input = strings.TrimSpace(input)
-
-		// the router is what directs the get/post to the handlers
-		router := router{handlers: map[string] map[string] func(Context){}}
-		router.configureGET( "/", doGet)
-		router.configurePOST("/", doPost)
-		
 		doStuff(router, input)
 	}
 }
 // Everything below is the "framework" 
 
 func (r router) deferRequest(method string, path []string) {
-	context := Context{
-		fullPath : "",
-		request : Request{ method: method},
-	}
-	// handle error input where there is no path
-	if len (path)  > 0 {
-		context.fullPath = path[0]
-	}
+	// gin will have an internal way of constructing this.
+	c,_ := gin.CreateTestContext(CLIResponseWriter{})
 
 	paths, ok := r.handlers[method]
 	if !ok {
-		context.String( 405, "Method not allowed: \n", method)
+		c.String( 405, "Method not allowed: %v \n", method)
 		return
 	}
 	handler, ok := paths[path[0]]
 	if !ok {
-		context.String( 404, "Path not found: \n", path[0])
+		c.String( 404, "Path not found: %v \n", path[0])
 		return
 	}
-
-	handler(context)
+	c.Request, _ = http.NewRequest(http.MethodPost, path[0], bytes.NewBuffer([]byte("{}")))
+	handler(c)
 }
 
 type router struct {
-	handlers map[string] map[string] func(Context)
-}
-
-type Context struct {
-	request Request
-	fullPath string
-}
-
-func (c Context) FullPath() string {
-	return c.fullPath
-}
-
-func (c Context) String(code int, format string, values ...interface{}) {
-	fmt.Printf("Return code %v, %v\n", code, values)
+	handlers map[string] map[string] func(*gin.Context)
 }
 
 type Request struct {
 	method string
 }
 
-func (r router) configureGET(path string, f func(Context)) {
+// A couple of functions to impersonate the http writer
+type CLIResponseWriter struct {
+
+}
+
+func (w CLIResponseWriter) Header() http.Header {
+	return map[string][]string{}
+}
+func (w CLIResponseWriter) Write(b []byte) (int, error)  {
+	written, error := os.Stdout.Write(b)
+	// fmt.Println("Write")
+	return written, error
+}
+func (w CLIResponseWriter) WriteHeader(statusCode int) {
+	fmt.Printf("WriteHeader: %v\n ",statusCode)
+}
+
+// Back to "our" framework code
+func (r router) configureGET(path string, f func(*gin.Context)) {
 	handler, ok := r.handlers["GET"]
 	if !ok {
-		handler = map[string] func(Context){}
+		handler = map[string] func(*gin.Context){}
 		r.handlers["GET"] = handler
 	}
 	r.handlers["GET"][path] = f
 }
 
-func (r router) configurePOST(path string, f func(Context)) {
+func (r router) configurePOST(path string, f func(*gin.Context)) {
 	handler, ok := r.handlers["POST"]
 	if !ok {
-		handler = map[string] func(Context){}
+		handler = map[string] func(*gin.Context){}
 		r.handlers["POST"] = handler
 	}
 	r.handlers["POST"][path] = f
@@ -100,6 +106,10 @@ func (r router) configurePOST(path string, f func(Context)) {
 
 func doStuff(routes router, line string) {
 	tokens := strings.Split(line, " ")
+	if len(tokens) < 2 {
+		fmt.Printf("error: please provide at least 2 tokens on the CLI.\n")
+		return
+	}
 	routes.deferRequest(tokens[0], tokens[1:])
 
 }
