d287176e917158eb9d5b7da1b5d32ec605f812af
sweeneyb <tab126@gmail.com>
2024-08-17 19:45:13 +0000

    abstracted handler to use context object

--- main.go
+++ main.go
@@ -8,12 +8,12 @@ import (
 )
 
 // These are our handler functions.
-func doGet(what []string) {
-	fmt.Printf("GET: %v\n", what)
+func doGet(what Context) {
+	what.String(200, "GET: %v\n", what.FullPath())
 }
 
-func doPost(what []string) {
-	fmt.Printf("POST: %v\n", what)
+func doPost(what Context) {
+	what.String(200 , "POST: %v\n", what.FullPath())
 }
 
 // This is the "main" where we configure our framework to handle the CLI commands
@@ -26,47 +26,73 @@ func main() {
 		input = strings.TrimSpace(input)
 
 		// the router is what directs the get/post to the handlers
-		router := router{handlers: map[string] map[string] func([]string){}}
+		router := router{handlers: map[string] map[string] func(Context){}}
 		router.configureGET( "/", doGet)
 		router.configurePOST("/", doPost)
 		
 		doStuff(router, input)
 	}
 }
+// Everything below is the "framework" 
 
 func (r router) deferRequest(method string, path []string) {
+	context := Context{
+		fullPath : "",
+		request : Request{ method: method},
+	}
+	// handle error input where there is no path
+	if len (path)  > 0 {
+		context.fullPath = path[0]
+	}
+
 	paths, ok := r.handlers[method]
 	if !ok {
-		fmt.Println("unknown method")
+		context.String( 405, "Method not allowed: \n", method)
 		return
 	}
 	handler, ok := paths[path[0]]
 	if !ok {
-		fmt.Println("404 - Path not found.")
+		context.String( 404, "Path not found: \n", path[0])
 		return
 	}
-	handler(path)
 
+	handler(context)
 }
 
-// Everything below is the "framework" 
 type router struct {
-	handlers map[string] map[string] func([]string)
+	handlers map[string] map[string] func(Context)
 }
 
-func (r router) configureGET(path string, f func([]string)) {
+type Context struct {
+	request Request
+	fullPath string
+}
+
+func (c Context) FullPath() string {
+	return c.fullPath
+}
+
+func (c Context) String(code int, format string, values ...interface{}) {
+	fmt.Printf("Return code %v, %v\n", code, values)
+}
+
+type Request struct {
+	method string
+}
+
+func (r router) configureGET(path string, f func(Context)) {
 	handler, ok := r.handlers["GET"]
 	if !ok {
-		handler = map[string] func([]string){}
+		handler = map[string] func(Context){}
 		r.handlers["GET"] = handler
 	}
 	r.handlers["GET"][path] = f
 }
 
-func (r router) configurePOST(path string, f func([]string)) {
+func (r router) configurePOST(path string, f func(Context)) {
 	handler, ok := r.handlers["POST"]
 	if !ok {
-		handler = map[string] func([]string){}
+		handler = map[string] func(Context){}
 		r.handlers["POST"] = handler
 	}
 	r.handlers["POST"][path] = f
